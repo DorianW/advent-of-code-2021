4c4d10909d0fc9d23685e9264bc95849ce681bee
Dorian W <dorianwojda@MacBook-Air-von-Dorian.fritz.box>
2021-12-12 23:12:22 +0100

    add solution for day 1 part 2

--- 001/index.test.ts
+++ 001/index.test.ts
@@ -1,10 +1,17 @@
 import { fileToArrayNumber } from "../utils/fileReader";
 import path from "path";
-import { getIncreasedMeasurement } from "./index";
+import { getIncreasedMeasurement, getIncreasedMeasurementBlocks } from "./index";
 
-test("fake test", () => {
+test("day 1 part 1 test", () => {
   const myPath = path.join(__dirname, "./input.txt");
   const inputArray = fileToArrayNumber(myPath);
   const result = getIncreasedMeasurement(inputArray);
   expect(result).toBe(1559);
+});
+
+test("day 1 part 2 test", () => {
+  const myPath = path.join(__dirname, "./input.txt");
+  const inputArray = fileToArrayNumber(myPath);
+  const result = getIncreasedMeasurementBlocks(inputArray);
+  expect(result).toBe(1600);
 });
--- 001/index.ts
+++ 001/index.ts
@@ -1,4 +1,20 @@
 export const getIncreasedMeasurement = (input: Array<number>): number => {
+
   return input.filter((element, index, array) => element > array[index - 1])
     .length;
 };
+
+export const getIncreasedMeasurementBlocks = (input: Array<number>): number => {
+  let counter = 0;
+  let match = 0;
+  while (counter <= input.length -3) {
+    const blockA = input.slice(counter, counter +3).reduce((a,b) => a + b);
+    const blockB = input.slice(counter +1, counter +4).reduce((a,b) => a + b);
+    if (blockB > blockA ) match++
+    counter++;
+  }
+  return match;
+  return input.filter((element, index, array) => index <= array.length - 3 && array.slice(index, index +3).reduce((first, second) => first + second) > array.slice(index + 1, index +4).reduce((first, second) => first + second))
+    .length;
+};
+
