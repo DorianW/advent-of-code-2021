923e3be18f994e7073265d49a929998e65a4a4bc
Dorian W <dorianwojda@MacBook-Air-von-Dorian.fritz.box>
2021-12-12 23:14:45 +0100

    optimize solution for day 1 part 2

--- 001/index.ts
+++ 001/index.ts
@@ -5,16 +5,7 @@ export const getIncreasedMeasurement = (input: Array<number>): number => {
 };
 
 export const getIncreasedMeasurementBlocks = (input: Array<number>): number => {
-  let counter = 0;
-  let match = 0;
-  while (counter <= input.length -3) {
-    const blockA = input.slice(counter, counter +3).reduce((a,b) => a + b);
-    const blockB = input.slice(counter +1, counter +4).reduce((a,b) => a + b);
-    if (blockB > blockA ) match++
-    counter++;
-  }
-  return match;
-  return input.filter((element, index, array) => index <= array.length - 3 && array.slice(index, index +3).reduce((first, second) => first + second) > array.slice(index + 1, index +4).reduce((first, second) => first + second))
+  return input.filter((element, index, array) => index <= array.length - 3 && array.slice(index, index +3).reduce((first, second) => first + second) < array.slice(index + 1, index +4).reduce((first, second) => first + second))
     .length;
 };
 
